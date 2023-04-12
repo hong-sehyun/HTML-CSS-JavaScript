8066d493e5e9ef744ed7e9003abd12369753bcbe
hong-sehyun <sese0733@naver.com>
2023-04-12 12:44:04 +0900

    섭씨화씨 변환

---
+++ Scripts/17.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sel1 = document.querySelector('#sel1');
+    const sel2 = document.querySelector('#sel2');
+
+    const d1 = document.querySelector('#d1')
+    const d2 = document.querySelector('#d2')
+
+
+    //단위표시
+    d1.textContent = sel1.value;
+    d2.textContent = sel2.value;
+
+
+    //select 값이 변경이 될때
+    sel1.addEventListener('change', (event) => {
+        d1.textContent = event.target.value;
+        t1.value = '';
+        t2.value = '';
+    });
+
+    sel2.addEventListener('change', (event) => {
+        d2.textContent = event.target.value;
+        t1.value = '';
+        t2.value = '';
+  
+    });
+
+
+    //input에 값이 입력되었을때
+    t1.addEventListener('input', (event) => {
+        console.log(event.target.value);
+
+        if(sel1.value == sel2.value){
+            t2.value = t1.value;
+        }
+
+        else if (sel1.value == "℃" && sel2.value == "℉"){
+
+        t2.value = (t1.value*(9/5)) + 32;
+        }
+        else if (sel1.value == "℉" && sel2.value == "℃"){
+            t2.value = (t1.value-32)*5/9;
+        }
+
+
+    });
+
+});
