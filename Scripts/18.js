f2eb2f93778d14a674267af35946d026f97b207e
hong-sehyun <sese0733@naver.com>
2023-04-13 12:16:18 +0900

    문자열 숫자합계

--- Scripts/18.js
+++ Scripts/18.js
@@ -26,8 +26,6 @@ const palindrome = (t1, t2) => {
     // console.log("s=", s, ", rs=", rs);
 
 
-
-
     //방법2 : 배열이용
     s = s.split('');  //공백없이 따옴표 붙여쓰면 단어 쪼개줌. 문자열 split() 메서드는 문자열을 분리자로 분리해서 배열 만듦
     s = s.reverse();  // 배열.reverse(); 배열의 요소를 뒤집기
@@ -42,25 +40,32 @@ const palindrome = (t1, t2) => {
 
 
     //결과확인
-    if (t1.value ==rs ) {
-        t1.value = "회문입니다";
+    if (t1.value == rs) {
+        t2.value = "회문입니다";
     } else {
         t2.value = "회문이 아닙니다"
     }
 }
 
+
+//숫자합계
 const numSum = (t1, t2) => {
-    let t = t2.value;
-    console.log(numSum);
+    let s = t1.value;
+    console.log(s)
+
+    //문자열 순회
+    let sum = 0;
+    for (let ch of s) {
+
+        if (!isNaN(ch)) sum = sum + parseInt(ch);
+
+
+        console.log(ch, isNaN(ch));
+    }
+    t2.value = sum;
+
+
 }
-
-
-
-
-
-
-
-
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -83,7 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for (let bt of bts) {
         //console.log(bt); // 개발자 도구에서 버튼이 두번 찍히는지 확인
-        bt1.addEventListener('click', (event) => {
+        bt.addEventListener('click', (event) => {
             event.preventDefault();
             //let gubun = bt.innerHTML ;
             //console.log(gubun); 
@@ -93,7 +98,6 @@ document.addEventListener("DOMContentLoaded", () => {
             // bt1의 '1'만 가져오거나 bt2의 '2'만 가져오기 위해
             // 이 기능을 안쓰면 getlength를 활용해야됨
 
-  
 
             if (gubun == 1) palindrome(t1, t2);
             //데이터 타입까지 보려면 (gubun === '1')
